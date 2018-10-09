51e20356dcd60a9e4a66d99b0df8daa004f843b3
Lucas Stoller <lucasstoller@hotmail.com>
2018-10-09 11:35:25 -0300

    Build generated

---
+++ mobile/app/build/generated/source/buildConfig/minApi21Demo/debug/com/todobom/opennotescanner/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.todobom.opennotescanner;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.todobom.opennotescanner.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "minApi21Demo";
+  public static final int VERSION_CODE = 10030;
+  public static final String VERSION_NAME = "1.0.30-minApi21-demo";
+  public static final String FLAVOR_api = "minApi21";
+  public static final String FLAVOR_mode = "demo";
+}
